288fda6f0c894876268884e4e4a8cda03aa02fbc
Leon Dong <LeonDong1993@gmail.com>
2020-07-27 19:10:08 -0500

    add bash script for creating partial data used in our paper

---
+++ classification/fdata/create.sh
@@ -0,0 +1,4 @@
+python create_partial_data.py cvx21.pkl cvx13.pkl 3,5
+python create_partial_data.py cvx21.pkl cvx9.pkl 1,3,5
+python create_partial_data.py pca21.pkl pca13.pkl 3,5
+python create_partial_data.py pca21.pkl pca9.pkl 1,3,5
